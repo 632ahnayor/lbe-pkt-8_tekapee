973e707236329c796a8663c17259c5b15d4f9c1a
632ahnayor <royan.habibi236@gmail.com>
2025-10-09 18:39:12 +0700

    supply=demand tiebreaker pilih cost lebih kecil

--- 8_tekapee.cpp
+++ 8_tekapee.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 
 int main() {
-    //NWM
+    //NWM 
+    //supply=demand tiebreaker pilih cost lebih kecil
 
     // cek semua file dalam folder "input"
     for(const auto& entry : filesystem::directory_iterator("input")) {
@@ -49,26 +50,36 @@ int main() {
             // loop runs until it reaches the bottom right corner
             while (startR != grid.size() && startC != grid[0].size()) {
                 // if demand is greater than supply
-                if (supply[startR] <= demand[startC]) {
+                if (supply[startR] < demand[startC]) {
                     ans += supply[startR] * grid[startR][startC];
                     // subtract the value of supply from the demand
                     demand[startC] -= supply[startR];
                     startR += 1;
                 }
                 // if supply is greater than demand
-                else {
+                else if (supply[startR] < demand[startC]){
                     ans += demand[startC] * grid[startR][startC];
                     // subtract the value of demand from the supply
                     supply[startR] -= demand[startC];
                     startC += 1;
                 }
+                // tie breaker supply=demand
+                else {
+                    ans += demand[startC] * grid[startR][startC];
+                    supply[startR]=0; demand[startC]=0;
+
+                    int costDown = (startR+1 < rows) ? grid[startR+1][startC] : INT_MAX;
+                    int costRight = (startC+1 < cols) ? grid[startR][startC+1] : INT_MAX;
+                    
+                    (costDown<=costRight) ? startR++ : startC++;
+                }
             }
             
-            cout << "Optimal solution is " << optimal << endl;
-            cout << "The initial feasible basic solution is " << ans << endl;
-            cout << "Difference " << ans-optimal << endl;
-            cout << "Accuracy " << (double(ans)-double(optimal))/double(optimal) << endl << endl;
-            
+            cout << ans << endl;
+            // cout << "Optimal solution is " << optimal << endl;
+            // cout << "The initial feasible basic solution is " << ans << endl;
+            // cout << "Difference " << ans-optimal << endl;
+            // cout << "Error " << (double(ans)-double(optimal))/double(optimal) << endl << endl;
         }
     }
 
